38d018f9fff57bc9e5e7be555d391393a44c219a
zeyaul98 <zeyaul445@gmail.com>
2026-03-10 12:53:57 +0530

    add new

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,17 +1,30 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import path from 'path';
 dotenv.config();
 
 import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js';
 
 const app = express();
+const __dirname = path.resolve();
 const PORT = process.env.PORT || 3000;
 
 // Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
 
+
+
+//make ready for production
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../Frontend/dist')));
+}
+
+
+app.get('*', (_, res) => {
+    res.sendFile(path.join(__dirname, '../Frontend/dist/index.html'));
+});
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
