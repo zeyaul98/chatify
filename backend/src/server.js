e5ee926127c6516748ba26567cd17be0dadc570e
zeyaul98 <zeyaul445@gmail.com>
2026-03-16 12:17:13 +0530

    fix frontend path

--- backend/src/server.js
+++ backend/src/server.js
@@ -18,12 +18,12 @@ app.use("/api/message", messageRoutes);
 
 //make ready for production
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../Frontend/dist')));
+    app.use(express.static(path.join(__dirname, '../frontend/dist')));
 }
 
 
 app.get('*', (_, res) => {
-    res.sendFile(path.join(__dirname, '../Frontend/dist/index.html'));
+    res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
 });
 
 app.get('/', (req, res) => {
