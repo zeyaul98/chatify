a839aa5fafa183246c240b19142c95af14926591
zeyaul98 <zeyaul445@gmail.com>
2026-03-18 11:35:27 +0530

    SIGNUP SECTION CODE REFACTORING

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -5,6 +5,10 @@ dotenv.config();
 
 export const connectDB = async () => {
     try{
+        const { MONGO_URL } = process.env;
+        if(!MONGO_URL){
+            throw new Error('MONGO_URL is not defined in environment variables');
+        }
         const conn = await mongoose.connect(process.env.MONGO_URL);
         console.log('MongoDB connected successfully', conn.connection.host);
     } catch (error) {
