5d6657ed362bd718f8517e8be1ca607290a001e5
zeyaul98 <zeyaul445@gmail.com>
2026-04-21 22:19:47 +0530

    add hooks

--- frontend/src/hooks/useKeyboardSound.js
+++ frontend/src/hooks/useKeyboardSound.js
@@ -1,19 +1,20 @@
+// audio setup
 const keyStrokeSounds = [
-    new Audio('/sound/keystroke1.mp3'),
-    new Audio('/sound/keystroke2.mp3'),
-    new Audio('/sound/keystroke3.mp3'),
-    new Audio('/sound/keystroke4.mp3'),
-]
+  new Audio("/sounds/keystroke1.mp3"),
+  new Audio("/sounds/keystroke2.mp3"),
+  new Audio("/sounds/keystroke3.mp3"),
+  new Audio("/sounds/keystroke4.mp3"),
+];
 
 function useKeyboardSound() {
-    const playRandomKeyStrokeSound = () => {
-        const randomSound = keyStrokeSounds[Math.floor(Math.random() * keyStrokeSounds.length)];
-        randomSound.currentTime = 0; // Reset to the beginning of the sound
-        randomSound.play().catch((error) => {            
-            console.error('Error playing sound:', error);
-        });
-    };
-    return {playRandomKeyStrokeSound};
+  const playRandomKeyStrokeSound = () => {
+    const randomSound = keyStrokeSounds[Math.floor(Math.random() * keyStrokeSounds.length)];
+
+    randomSound.currentTime = 0; // this is for a better UX, def add this
+    randomSound.play().catch((error) => console.log("Audio play failed:", error));
+  };
+
+  return { playRandomKeyStrokeSound };
 }
 
-export default useKeyboardSound
+export default useKeyboardSound;
