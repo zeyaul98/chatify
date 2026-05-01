88b7acdecf617043b3c304a76f4abd819627e5ac
zeyaul98 <zeyaul445@gmail.com>
2026-05-01 15:15:02 +0530

    change  pageloader

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ import ChatPage from "./Pages/ChatPage"
 import { useAuthStore } from "./store/useAuthStore"
 import { useEffect } from "react"
 import { Navigate } from "react-router-dom"
-import PageLoader from "./Components/PageLoader"
+import PageLoader from "./components/PageLoader"
 import {Toaster} from 'react-hot-toast'
 
 
